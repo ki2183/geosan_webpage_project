51a0316d92489c75f14f0f160c24a5117de075d4
ki2183 <ki2183@naver.com>
2024-06-04 10:05:19 +0900

    git_remake_main

--- app/ui/main/mainBannerTest.tsx
+++ app/ui/main/mainBannerTest.tsx
@@ -4,6 +4,7 @@ import plan from "../../IMGs/mainbanner/plan.png"
 import plan1 from "../../IMGs/mainbanner/plan1.png"
 import plan2 from "../../IMGs/mainbanner/plan2.png"
 import pattern from "../../IMGs/mainbanner/parttern.png"
+import cnc from "../../IMGs/mainbanner/cnc_.png"
 
 export default function MainBanner(){
 
@@ -13,6 +14,15 @@ export default function MainBanner(){
         <section id="main-banner">
             <div style={{height:"70vh"}} className=" relative noto-sans-kr w-screen bg-main pt-8 flex item-center justify-center">
                 <div style={{backgroundImage: `url(${pattern.src})`, opacity:0.02}} className="absolute w-full h-full top-0"/>
+                {/* <div style={{backgroundImage: `url(${cnc.src})`, opacity:1}} className="absolute w-full h-full top-0"/> */}
+
+                <Image
+                    alt="img"
+                    src={cnc}
+                    className="absolute right-0 w-full "
+                />
+                
+                
                 <div style={{maxWidth:"1080px"}} className="h-full w-3/4 grid grid-cols-2">
                     <div style={{maxWidth:"420px"}} className="flex flex-col items-star justify-center text-orange-50 gap-5">
                         <h1 className="text-5xl font-bold">
@@ -27,7 +37,7 @@ export default function MainBanner(){
                     </div>
                     <div className="flex items-center justify-center" >
                         <div className="w-screen h-full flex relative items-center justify-center">
-                            <div className="relative">
+                            <div className="relative w-full">
                                 <Image
                                     src={plan}
                                     className="ml-auto w-full opacity-50"
@@ -47,7 +57,7 @@ function Mainbutton(){
     const text_color = "#1D3A44"
     return(
         <button style={{color:text_color}} className="bg-red-50 p-1 pl-3 pr-3 rounded-full font-bold text-gray-600">
-            기술 보기
+            지도 보기
         </button>
     )
 }
