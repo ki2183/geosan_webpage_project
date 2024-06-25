cb70c29021fdef17813bb4bde628fe19e814b340
ki2183 <ki2183@naver.com>
2024-06-25 10:16:52 +0900

    temporary_api

--- app/fetch/fetchVendorData/fetchVendorData.ts
+++ app/fetch/fetchVendorData/fetchVendorData.ts
@@ -7,11 +7,18 @@ export type fetch_vendor_data_type = {
 };
 
 export default async function fetch_vendor_data() {
-    try {
-        const data = await sql<fetch_vendor_data_type[]>`SELECT * FROM vendor`;
-        return data.rows
-    } catch (err) {
-        console.error("Database Error:", err);
-        throw new Error("Failed to fetch the latest fetch_cnc_data.");
-    }
+    // try {
+    //     const data = await sql<fetch_vendor_data_type[]>`SELECT * FROM vendor`;
+    //     return data.rows
+    // } catch (err) {
+    //     console.error("Database Error:", err);
+    //     throw new Error("Failed to fetch the latest fetch_cnc_data.");
+    // }
+    return [
+        {
+            id:0,
+            category:'kk',
+            companyname:'kk'
+        }
+    ]
 }
