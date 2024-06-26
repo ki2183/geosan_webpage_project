518933ac94da0f50650008b54972b3c8fd4a9de5
ki2183 <ki2183@naver.com>
2024-06-26 09:10:30 +0900

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import { Metadata } from "next";
 
-
-
 export const metadata: Metadata = {
-	title: "거산테크",
-  description:"산업기계 설비, 반도체 장비 부품, 소재, 정밀가공,MCT",
+	title: "거산테크 -CNC 선반 정밀가공 업체",
+  description:"산업기계 설비, 반도체 장비 부품, 소재, 정밀가공, MCT, 시화공단, 안산 시화공단",
   authors:[{ name: "김기준", url: "https://github.com/ki2183" }],
-  keywords:"산업기계 설비, 반도체 장비 부품, 소재, 정밀가공,MCT",
+  keywords:"산업기계 설비, 반도체 장비 부품, 소재, 정밀가공,MCT, 시화공단, 안산 시화공단",
+  
 	icons: {
 		icon: "/geosan_logo.webp",
 	},
